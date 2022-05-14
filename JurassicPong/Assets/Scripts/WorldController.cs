017d15807d84a6b41ab0a656ee5ee2100f70bb88
Rocco Briganti <rocco_brig@hotmail.com>
2022-05-14 11:44:34 -0700

    Added world seam
    World seam and trigger moves with score

--- JurassicPong/Assets/Scripts/WorldController.cs
+++ JurassicPong/Assets/Scripts/WorldController.cs
@@ -5,13 +5,15 @@ using UnityEngine.UI;
 
 public class WorldController : MonoBehaviour
 {
-    public static readonly Vector4 WORLD_BOUNDS = new Vector4(0.5f, 17.28371f, 1f, 9f);
+    public static readonly Vector4 WORLD_BOUNDS = new Vector4(0.5f, 17.78371f, 1f, 9f);
 
     [Header("Backgrounds")]
     [SerializeField] private RectTransform _backgroundCanvas;
     [SerializeField] private Image _leftWorldBackground;
     [SerializeField] private Image _rightWorldBackground;
     [SerializeField] private Image _worldSeam;
+    [SerializeField] private Transform _worldSeamCollider;
+
 
     [Header("Scoring")]
     public int MaxScoreDifference = 10;
@@ -32,25 +34,15 @@ public class WorldController : MonoBehaviour
     {
         _percentagePerPoint = (1f / MaxScoreDifference * MaxScreenPercentage) / 100f;
         // Player one score.
-        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             ScorePlayerOne(1);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            ScorePlayerOne(-1);
-        }
-
         // Player two scoring.
-        if (Input.GetKey(KeyCode.UpArrow) && Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             ScorePlayerTwo(1);
-        }
-        
-        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            ScorePlayerTwo(-1);
         }
     }
 
@@ -74,12 +66,14 @@ public class WorldController : MonoBehaviour
             _leftWorldBackground.fillAmount += movementPerc;
             _rightWorldBackground.fillAmount -= movementPerc;
             _worldSeam.rectTransform.anchoredPosition += new Vector2(_backgroundCanvas.sizeDelta.x * movementPerc, 0f);
+            _worldSeamCollider.transform.position += new Vector3(WORLD_BOUNDS.y * movementPerc, 0f, 0f);
         }
         else
         {
             _leftWorldBackground.fillAmount -= movementPerc;
             _rightWorldBackground.fillAmount += movementPerc;
             _worldSeam.rectTransform.anchoredPosition -= new Vector2(_backgroundCanvas.sizeDelta.x * movementPerc, 0f);
+            _worldSeamCollider.transform.position -= new Vector3(WORLD_BOUNDS.y * movementPerc, 0f, 0f);
         }
     }
 }
