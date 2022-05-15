22ea9f47acfa3f6b3f2b246dc08ce939f291eca2
Galen Manuel <galen.manuel@gmail.com>
2022-05-15 16:24:41 -0400

    Particles are spawned when they hit portals

--- JurassicPong/Assets/Scripts/Portal.cs
+++ JurassicPong/Assets/Scripts/Portal.cs
@@ -13,9 +13,14 @@ public class Portal : MonoBehaviour
 
     public Side WorldSide;
     public Action<Side, Thing> Scored;
+    public ParticleSystem GoodSystemToSpawnOnScore;
+    public ParticleSystem BadSystemToSpawnOnScore;
 
     private void Awake()
     {
+        GameHelper.IsNull(GoodSystemToSpawnOnScore);
+        GameHelper.IsNull(BadSystemToSpawnOnScore);
+
         Subscribe();
     }
 
@@ -29,7 +34,23 @@ public class Portal : MonoBehaviour
         Messenger.RemoveListener(Events.GAME_OVER, OnGameOver);
     }
 
+    private void SpawnParticles(ParticleSystem systemToSpawn, Vector3 position)
+    {
+        ParticleSystem system = Instantiate(systemToSpawn, position, Quaternion.identity);
+        var vel = system.velocityOverLifetime;
+        vel.x = WorldSide == Side.Right ? new ParticleSystem.MinMaxCurve(vel.x.constantMin * -1, vel.x.constantMax * -1) : vel.x;
+    }
+
     #region Event Handlers
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var thing = collision.transform.root.GetComponent<Thing>();
+        if (thing)
+        {
+            SpawnParticles(thing is BadThing ? BadSystemToSpawnOnScore : GoodSystemToSpawnOnScore, collision.ClosestPoint(thing.transform.position));
+        }
+    }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
@@ -38,6 +59,8 @@ public class Portal : MonoBehaviour
         {
             Messenger.Broadcast(Events.PORTAL_SCORED, WorldSide, thing, MessengerMode.REQUIRE_LISTENER);
             Messenger.Broadcast(Events.DESTROY_THING, thing, MessengerMode.REQUIRE_LISTENER);
+
+            SpawnParticles(thing is BadThing ? BadSystemToSpawnOnScore : GoodSystemToSpawnOnScore, thing.transform.position);
         }
     }
 
